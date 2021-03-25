fa3527ed69b50f42f97c6773c18b0ce54e023697
Ronald Quichimbo <ronal1923@gmail.com>
2021-03-25 17:53:42 -0500

    actualice gateway

--- gateway/app.py
+++ gateway/app.py
@@ -6,11 +6,11 @@ from auth import jwt
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Bruce Wayne is Batman'
+app.config['SECRET_KEY'] = 'tesis_devsecops'
 jwt.init_app(app)
 
-MOVIES_SERVICE_URL = 'http://movies:5001'
-REVIEWS_SERVICE_URL = 'http://reviews:5002'
+MOVIES_SERVICE_URL = 'http://172.20.0.1:5001'
+REVIEWS_SERVICE_URL = 'http://172.20.0.1:5002'
 
 
 @app.route('/movies/')
