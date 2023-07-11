cc5d0b1513fa1c68968a5856a2e2a0b64e6cdc49
ronald quichimbo <ronal1923@gmail.com>
2023-07-11 09:48:18 -0500

    db

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ DB_NAME = 'Product'
 user='tesis'
 password = 'devsecops2023'
 
-uri = "mongodb+srv://{user}:{password}@tesisdevsecops2023.wdmpqj0.mongodb.net/?retryWrites=true&w=majority"
+uri= "mongodb+srv://{user}:{password}@tesisdevsecops2023.wdmpqj0.mongodb.net/?retryWrites=true&w=majority"
 
 connection = MongoClient(uri, server_api=ServerApi('1'))
 db_client = connection[DB_NAME]
